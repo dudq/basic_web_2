56a9c593fa96a28e46d2a8d1b6efabaffeac73ac
dudq <dudq.xmt@gmail.com>
2019-11-12 14:21:37 +0700

    add simple_dictionary file

---
+++ simple_dictionary/src/simpleDictionary/SimpleDictionaryServlet.java
@@ -0,0 +1,41 @@
+package simpleDictionary;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "SimpleDictionaryServlet", urlPatterns = "/translate")
+public class SimpleDictionaryServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chao");
+        dictionary.put("how", "The nao");
+        dictionary.put("book", "Quyen vo");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("<html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
